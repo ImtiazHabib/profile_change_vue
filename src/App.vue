3434c21ff35a12bf7ba03c0aec2938706b36e1e7
ImtiazHabib <imtiazhabib7@gmail.com>
2025-11-17 21:50:16 +0600

    Bootstrap add ont he project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {reactive} from 'vue';
+
+const users = [
+  {
+    id:1,
+    name:"imtiaz habib",
+    description:"hello I am imtiaz habib and I love black",
+    color:"black",
+    img:"https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg",
+  },
+   {
+    id:1,
+    name:"Arman Malik",
+    description:"hello I am Arman Malik and I love red",
+    color:"red",
+    img:"https://images.pexels.com/photos/10392356/pexels-photo-10392356.jpeg",
+  }
+];
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <section class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Welcome to Profile change vue</h1>
+      </div>
+    </div>
+
+   </section>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
